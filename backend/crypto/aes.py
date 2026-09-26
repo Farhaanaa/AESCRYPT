d6edea26d3b78a55a8290cd9cc6024fa744206a6
Farhaanaa <fathimafarhanakn@gmail.com>
2026-09-26 14:34:02 +0530

    feat: implement AES-CBC encryption and decryption

--- backend/crypto/aes.py
+++ backend/crypto/aes.py
@@ -1,3 +1,5 @@
+import os
+
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
 
@@ -27,6 +29,43 @@ def decrypt_ecb(ciphertext, key):
     cipher = Cipher(
         algorithms.AES(key),
         modes.ECB()
+    )
+
+    decryptor = cipher.decryptor()
+    padded_data = decryptor.update(ciphertext) + decryptor.finalize()
+
+    # Remove PKCS7 padding
+    unpadder = padding.PKCS7(BLOCK_SIZE).unpadder()
+    data = unpadder.update(padded_data) + unpadder.finalize()
+
+    return data
+
+
+def encrypt_cbc(data, key):
+    # PKCS7 padding
+    padder = padding.PKCS7(BLOCK_SIZE).padder()
+    padded_data = padder.update(data) + padder.finalize()
+
+    # Generate a random 16-byte IV
+    iv = os.urandom(16)
+
+    # AES-CBC
+    cipher = Cipher(
+        algorithms.AES(key),
+        modes.CBC(iv)
+    )
+
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+
+    return iv, ciphertext
+
+
+def decrypt_cbc(iv, ciphertext, key):
+    # AES-CBC
+    cipher = Cipher(
+        algorithms.AES(key),
+        modes.CBC(iv)
     )
 
     decryptor = cipher.decryptor()
